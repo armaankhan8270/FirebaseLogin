cdf2d00887617ed63ab48a506ca539e1bdc41ceb
armaan <karmankkhan@gmail.com>
2023-11-16 09:47:12 +0530

    done

--- js/config/config.js
+++ js/config/config.js
@@ -4,15 +4,24 @@
  * @license MIT
  */
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCKGUmFr5B7Uag0NctkhCRDkAQoB5pYNQg",
-  authDomain: "fir-login-2eaf4.firebaseapp.com",
-  databaseURL: "https://fir-login-2eaf4-default-rtdb.firebaseio.com",
-  projectId: "fir-login-2eaf4",
-  storageBucket: "fir-login-2eaf4.appspot.com",
-  messagingSenderId: "608621681488",
-  appId: "1:608621681488:web:626e37a5cc92b8764aaa55",
-  measurementId: "G-T70T1M6SSE"
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCKGUmFr5B7Uag0NctkhCRDkAQoB5pYNQg",
+//   authDomain: "fir-login-2eaf4.firebaseapp.com",
+//   databaseURL: "https://fir-login-2eaf4-default-rtdb.firebaseio.com",
+//   projectId: "fir-login-2eaf4",
+//   storageBucket: "fir-login-2eaf4.appspot.com",
+//   messagingSenderId: "608621681488",
+//   appId: "1:608621681488:web:626e37a5cc92b8764aaa55",
+//   measurementId: "G-T70T1M6SSE"
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBseIS1yJPrUub6WApe0uTpN-IZ-68dC5k",
+  authDomain: "attandance-manger.firebaseapp.com",
+  projectId: "attandance-manger",
+  storageBucket: "attandance-manger.appspot.com",
+  messagingSenderId: "770790542382",
+  appId: "1:770790542382:web:a074775950dcccf32b63bf",
+  measurementId: "G-E8N3RBR5M8",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
